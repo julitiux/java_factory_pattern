163b86b830f8bedd07af412c9863c3de1024dba5
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-08-20 20:03:04 -0600

    starting new project java_factory_pattern

---
+++ src/main/java/com/java_factory_pattern/JavaFactoryPatternApplication.java
@@ -0,0 +1,13 @@
+package com.java_factory_pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFactoryPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFactoryPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java_factory_pattern/JavaFactoryPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_factory_pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFactoryPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
